74831f9b7de679ed1bc530d8fb0a0e407a082fa1
daadesina <daadesina1@gmail.com>
2024-09-27 02:12:39 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import './src_bootstrap.css'
+import './my_home.css';
+import 'https://kit.fontawesome.com/c5e4d5e6fa.js';
+import Home from './my_home';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home/>
     </div>
   );
 }
